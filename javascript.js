357a3bd241d93c5618af69aa53c929b4ece9bd4e
pwindrap18 <pwindrap18@gmail.com>
2021-11-14 11:37:41 +0700

    add function playRound

--- javascript.js
+++ javascript.js
@@ -3,4 +3,18 @@ const computerPlay = () => {
   return rpc[Math.floor(Math.random() * rpc.length)];
 };
 
-console.log(computerPlay());
+const playRound = (player, computer) => {
+  const lower = player.toLowerCase();
+  switch (lower === "rock") {
+    case computer === "rock":
+      return "its a draw";
+    case computer === "paper":
+      return "you lose";
+    case computer === "scissors":
+      return "you win";
+  }
+};
+
+const playerSelect = "rock";
+const computerSelect = computerPlay();
+console.log(playRound(playerSelect, computerSelect));
